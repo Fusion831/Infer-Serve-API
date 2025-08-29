3a3555a2c30b21020ada7c73110e9b0d0a0bc890
Daksh <dakshdaksh698@gmail.com>
2025-08-29 22:06:14 +0530

    Added tests for mocking API

--- tests/test_main.py
+++ tests/test_main.py
@@ -24,3 +24,25 @@ def test_predict_malformed():
         payload = {"sepal_length": "invalid_data"} 
         response = client.post("/predict", json=payload)
         assert response.status_code == 422
+
+def test_predict_rate_limit_exceeded(mocker):
+    
+    mock_redis_incr = mocker.patch("redis.Redis.incr")
+    
+    
+    mock_redis_incr.return_value = 11 
+
+    
+    mocker.patch("redis.Redis.expire")
+    
+    with TestClient(app) as client:
+        
+        response = client.post("/predict", json={
+            "sepal_length": 5.1,
+            "sepal_width": 3.5,
+            "petal_length": 1.4,
+            "petal_width": 0.2
+        })
+        
+        
+        assert response.status_code == 429
